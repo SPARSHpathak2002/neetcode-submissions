0294850a75fb4e915f4107d6be28e66a36af6987
Sparsh Pathak <69711061+SPARSHpathak2002@users.noreply.github.com>
2026-05-16 11:39:33 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+       int n = nums.Length;
+
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            int product = 1;
+
+            for (int j = 0; j < n; j++) {
+
+                if (i != j) {
+                    product *= nums[j];
+                }
+            }
+
+            ans[i] = product;
+        }
+
+        return ans;
+    }
+}
