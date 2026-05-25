a5396d0e77e3db95c04adfe6827eaf37878968f4
Sparsh Pathak <69711061+SPARSHpathak2002@users.noreply.github.com>
2026-05-25 22:52:11 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        var uniqueTriplets = new HashSet<string>();
+        var result = new List<List<int>>();
+
+        int n = nums.Length;
+
+        // Loop 1: First element
+        for (int i = 0; i < n - 2; i++) {
+            // Loop 2: Second element
+            for (int j = i + 1; j < n - 1; j++) {
+                // Loop 3: Third element
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        // Create and sort the triplet to create a unique key
+                        var triplet = new List<int> { nums[i], nums[j], nums[k] };
+                        triplet.Sort();
+
+                        // Create a unique string key (e.g., "-1,0,1")
+                        string key = $"{triplet[0]},{triplet[1]},{triplet[2]}";
+
+                        // If this triplet layout hasn't been added yet, save it
+                        if (!uniqueTriplets.Contains(key)) {
+                            uniqueTriplets.Add(key);
+                            result.Add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
